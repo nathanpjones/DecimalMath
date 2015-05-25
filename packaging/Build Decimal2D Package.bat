8ebe121231a34f16e384ad206261b86484dd28db
Nathan Jones <nathan@nathanpjones.com>
2015-05-25 14:23:51 -0700

    Revise package description and build scripts.

--- packaging/Build Decimal2D Package.bat
+++ packaging/Build Decimal2D Package.bat
@@ -2,6 +2,7 @@
 echo ------------------------------------
 echo Building Decimal2D package...
 echo ------------------------------------
+if not exist "%~dp0nuget" md "%~dp0nuget"
 nuget pack "%~dp0..\Decimal2D\Decimal2D.csproj" -build -Prop Configuration=Release -OutputDirectory "%~dp0nuget" -IncludeReferencedProjects -Symbols
 echo.
 pause
--- packaging/Build DecimalEx Package.bat
+++ packaging/Build DecimalEx Package.bat
@@ -2,6 +2,7 @@
 echo ------------------------------------
 echo Building DecimalEx package...
 echo ------------------------------------
+if not exist "%~dp0nuget" md "%~dp0nuget"
 nuget pack "%~dp0..\DecimalEx\DecimalEx.csproj" -build -Prop Configuration=Release -OutputDirectory "%~dp0nuget" -Symbols
 echo.
 pause
