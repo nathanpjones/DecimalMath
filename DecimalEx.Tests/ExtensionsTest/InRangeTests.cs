d11fc185a1c7af94cb214c3eb0e1a1c6b0caf501
Guido Leenders <guido.leenders@invantive.com>
2018-06-09 13:57:02 +0200

    Upgrade from NUnit 2.6.4 to NUnit 3.10 and Visual Studio 2017.

--- DecimalEx.Tests/ExtensionsTest/InRangeTests.cs
+++ DecimalEx.Tests/ExtensionsTest/InRangeTests.cs
@@ -5,15 +5,12 @@ using NUnit.Framework;
 
 namespace DecimalExTests.ExtensionsTest
 {
-
     public class InRangeTests
     {
         public static IEnumerable InclusiveTestCases
         {
             get
             {
-                yield return new TestCaseData(0m, 4m, 1m).Throws(typeof(Exception)).SetName("Argument Test");
-
                 yield return new TestCaseData(3.5m, 1m, 4m).Returns(true);
                 yield return new TestCaseData(1m, 1m, 4m).Returns(true);
                 yield return new TestCaseData(4m, 1m, 4m).Returns(true);
@@ -28,12 +25,24 @@ namespace DecimalExTests.ExtensionsTest
             return value.InRangeIncl(lowerLimit, upperLimit);
         }
 
+        public static IEnumerable InclusiveTestCasesWithException
+        {
+            get
+            {
+                yield return new TestCaseData(0m, 4m, 1m);
+            }
+        }
+
+        [TestCaseSource("InclusiveTestCasesWithException")]
+        public void InclusiveTestsWithException(decimal value, decimal lowerLimit, decimal upperLimit)
+        {
+            Assert.That(() => value.InRangeIncl(lowerLimit, upperLimit), Throws.Exception);
+        }
+
         public static IEnumerable ExclusiveTestCases
         {
             get
             {
-                yield return new TestCaseData(0m, 4m, 1m).Throws(typeof(Exception)).SetName("Argument Test");
-
                 yield return new TestCaseData(3.5m, 1m, 4m).Returns(true);
                 yield return new TestCaseData(1m, 1m, 4m).Returns(false);
                 yield return new TestCaseData(4m, 1m, 4m).Returns(false);
@@ -46,6 +55,20 @@ namespace DecimalExTests.ExtensionsTest
         public bool ExclusiveTests(decimal value, decimal lowerLimit, decimal upperLimit)
         {
             return value.InRangeExcl(lowerLimit, upperLimit);
+        }
+
+        public static IEnumerable ExclusiveTestCasesWithException
+        {
+            get
+            {
+                yield return new TestCaseData(0m, 4m, 1m);
+            }
+        }
+
+        [TestCaseSource("ExclusiveTestCasesWithException")]
+        public void ExclusiveTestsWithException(decimal value, decimal lowerLimit, decimal upperLimit)
+        {
+            Assert.That(() => value.InRangeExcl(lowerLimit, upperLimit), Throws.Exception);
         }
     }
 
