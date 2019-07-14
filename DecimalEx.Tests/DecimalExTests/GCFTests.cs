2e860b9b50eb6cdf675b4ebcc7119d04f255794b
Nathan Jones <nathan@nathanpjones.com>
2019-07-13 20:48:12 -0700

    Simplify projects and package build
    Move metadata to project package properties and link package version to assembly version
    Update package build scripts to rely on project metadata and build in release mode
    Convert test libraries to .NET Core to match NUnit package requirements
    Fix unit test for GCF that had wrong expected value
    Resolves #2

--- DecimalEx.Tests/DecimalExTests/GCFTests.cs
+++ DecimalEx.Tests/DecimalExTests/GCFTests.cs
@@ -7,18 +7,19 @@ namespace DecimalExTests.DecimalExTests
 
     public class GCFTests
     {
+        // For verification, can use: https://www.omnicalculator.com/math/gcf
         public static IEnumerable TestCases
         {
             get
             {
                 yield return new TestCaseData(1.2m, 0.42m).Returns(.06m);
                 yield return new TestCaseData(1071m, 462m).Returns(21m);
-                yield return new TestCaseData(decimal.MaxValue / 1000m, .28m).Returns(.02m);
+                yield return new TestCaseData(decimal.MaxValue / 1000m, .28m).Returns(.035m);
                 yield return new TestCaseData(0.0000000000000000000000823543m, 0.0000000000019626617431640625m).Returns(0.0000000000000000000000000343m);
             }
         }
 
-        [TestCaseSource("TestCases")]
+        [TestCaseSource(nameof(TestCases))]
         public decimal Test(decimal a, decimal b)
         {
             return DecimalEx.GCF(a, b);
--- packaging/Build Decimal2D Package.bat
+++ packaging/Build Decimal2D Package.bat
@@ -3,6 +3,6 @@ echo ------------------------------------
 echo Building Decimal2D package...
 echo ------------------------------------
 if not exist "%~dp0nuget" md "%~dp0nuget"
-dotnet pack "%~dp0..\Decimal2D\Decimal2D.csproj" --configuration Debug --output "%~dp0nuget" --include-symbols /p:PackageVersion=1.0.1 "/p:Authors=Nathan P Jones" "/p:Owner=Nathan P Jones" /p:PackageLicenseUrl=https://github.com/nathanpjones/DecimalMath/blob/master/LICENSE /p:PackageProjectUrl=https://github.com/nathanpjones/DecimalMath /p:PackageRequireLicenseAcceptance=false "/p:Description=Portable math support for Decimal-based geometric and trigonometric calculations." "/p:Copyright=Copyright 2015 - Nathan P Jones" "/p:PackageTags=c# decimal .net math pcl trigonometry geometry"
+dotnet pack "%~dp0..\Decimal2D\Decimal2D.csproj" --configuration Release --include-symbols --output "%~dp0nuget"
 echo.
 pause
