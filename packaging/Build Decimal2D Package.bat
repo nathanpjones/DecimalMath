550e484c308f9f0076dbe853ee938d1917e67b3b
Nathan Jones <nathan@nathanpjones.com>
2019-07-14 11:29:44 -0700

    Update package build to use new snupkg format

--- packaging/Build Decimal2D Package.bat
+++ packaging/Build Decimal2D Package.bat
@@ -3,6 +3,6 @@ echo ------------------------------------
 echo Building Decimal2D package...
 echo ------------------------------------
 if not exist "%~dp0nuget" md "%~dp0nuget"
-dotnet pack "%~dp0..\Decimal2D\Decimal2D.csproj" --configuration Release --include-symbols --output "%~dp0nuget"
+dotnet pack "%~dp0..\Decimal2D\Decimal2D.csproj" --configuration Release --include-symbols --output "%~dp0nuget" -p:SymbolPackageFormat=snupkg
 echo.
 pause
--- packaging/Build DecimalEx Package.bat
+++ packaging/Build DecimalEx Package.bat
@@ -3,6 +3,6 @@ echo ------------------------------------
 echo Building DecimalEx package...
 echo ------------------------------------
 if not exist "%~dp0nuget" md "%~dp0nuget"
-dotnet pack "%~dp0..\DecimalEx\DecimalEx.csproj" --configuration Release --include-symbols --output "%~dp0nuget"
+dotnet pack "%~dp0..\DecimalEx\DecimalEx.csproj" --configuration Release --include-symbols --output "%~dp0nuget" -p:SymbolPackageFormat=snupkg
 echo.
 pause
