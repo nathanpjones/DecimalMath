bd9fbca5c2b6b3ad2ffa654933d00b6f5db4d42b
Nathan Jones <nathan@nathanpjones.com>
2019-07-14 11:50:53 -0700

    Update package build to include XML doc file

--- packaging/Build Decimal2D Package.bat
+++ packaging/Build Decimal2D Package.bat
@@ -3,6 +3,6 @@ echo ------------------------------------
 echo Building Decimal2D package...
 echo ------------------------------------
 if not exist "%~dp0nuget" md "%~dp0nuget"
-dotnet pack "%~dp0..\Decimal2D\Decimal2D.csproj" --configuration Release --include-symbols --output "%~dp0nuget" -p:SymbolPackageFormat=snupkg
+dotnet pack "%~dp0..\Decimal2D\Decimal2D.csproj" --configuration Release -p:GenerateDocumentationFile=true --include-symbols -p:SymbolPackageFormat=snupkg --output "%~dp0nuget"
 echo.
 pause
--- packaging/Build DecimalEx Package.bat
+++ packaging/Build DecimalEx Package.bat
@@ -3,6 +3,6 @@ echo ------------------------------------
 echo Building DecimalEx package...
 echo ------------------------------------
 if not exist "%~dp0nuget" md "%~dp0nuget"
-dotnet pack "%~dp0..\DecimalEx\DecimalEx.csproj" --configuration Release --include-symbols --output "%~dp0nuget" -p:SymbolPackageFormat=snupkg
+dotnet pack "%~dp0..\DecimalEx\DecimalEx.csproj" --configuration Release -p:GenerateDocumentationFile=true --include-symbols -p:SymbolPackageFormat=snupkg --output "%~dp0nuget"
 echo.
 pause
